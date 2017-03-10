a3357dced394af9fb6aab3aed7db3b96896416b4
Scorpio59 <rudywahl@gmail.com>
2017-03-10 21:12:50 +0100

    template ok

---
+++ src/mainWindow.js
@@ -0,0 +1,64 @@
+ class Views {
+       const webview = document.createElement('webview');
+
+    constructor() {
+      this.views = []
+    }
+
+    
+}
+
+class Column{
+
+}
+
+
+
+class Row{
+
+  constructor(parent) {
+      this.parentColumn = parent;
+      
+            this.columns = []
+        }
+
+addColumn(){
+    
+}
+
+move(direction){
+
+}
+
+remove()
+{
+
+}
+}
+
+class View{
+
+constructor(){
+    this.tabs = [],
+    this.rollingTime = 10
+
+}
+remove(){
+
+}
+
+
+}
+
+
+class Tab {
+
+
+    constructor(){
+this.url,
+this.needRefresh,
+this.contentSelector
+}
+
+
+}
