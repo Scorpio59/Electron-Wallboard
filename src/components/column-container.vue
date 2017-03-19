625fdc92e518a284243231d1453748e57f8175b3
Scorpio59 <rudywahl@gmail.com>
2017-03-19 14:14:03 +0100

    add column component

---
+++ src/components/column-container.vue
@@ -0,0 +1,32 @@
+<template>
+<div id="column-container" class="column"  :class="[isInEditMode ? 'edit':'']" ></div>
+</template>
+
+<script>
+export default {
+  name: 'column-container',
+  props: ['isInEditMode'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    };
+  }
+};
+</script>
+
+
+<style scoped>
+.column {
+   float: left;
+   flex: 1;
+    resize: horizontal;
+    overflow: hidden;
+    min-height: 100vh;
+}
+
+.edit{
+     border: 1px solid;
+     overflow: auto;
+}
+
+</style>
---
+++ src/components/web-view.vue
@@ -0,0 +1,20 @@
+<template>
+<webview :src="src"></webview>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    };
+  }
+};
+</script>
+
+
+<style scoped>
+
+
+</style>
