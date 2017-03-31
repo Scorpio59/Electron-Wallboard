964836b009fbd1705f2dcbc84e328638379bdb47
abanob <georges.abanob@gmail.com>
2017-03-31 18:06:50 +0200

    Update of configuration import and export feature

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const electron = require('electron');
 const {
-    app,
-    BrowserWindow,
-    Menu,
-    dialogt,
-    globalShortcut,
-    ipcMain
+	app,
+	BrowserWindow,
+	Menu,
+	dialog,
+	globalShortcut,
+	ipcMain
 } = electron;
 
 // import {app, BrowserWindow, Menu} from 'electron';
@@ -17,153 +17,156 @@ const fs = require('fs');
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 var isInEditMode = true;
+var configFileToSave;
 
 function toggleMode() {
-    isInEditMode = !isInEditMode;
-    win.webContents.send('edit-mode', isInEditMode);
+	isInEditMode = !isInEditMode;
+	win.webContents.send('edit-mode', isInEditMode);
 }
 
-function onConfigFileRead(err, data) {
-    var configuration;
-    if (err) throw err;
-    configuration = JSON.parse(data);
+/* Configuration Export */
+function saveConfig() {
+	dialog.showSaveDialog({
+		title: 'Export configuration in file',
+		filters: [{
+			name: 'Config file',
+			extensions: ['json']
+		}]
+	}, function(fileName) {
+		configFileToSave = fileName;
+		win.webContents.send('request-config');
+	});
 }
 
-app.on('requested-config', (evt, confPresets) => {
-    if (confPresets) {
-        var fileName = confPresets[0];
-        var presets = confPresets[1];
-        if (fileName === undefined) return;
-
-        fs.writeFile(fileName, presets, function(err) {
-            if (!err) {
-                dialog.showMessageBox({
-                    message: 'The file has been saved! :-)',
-                    buttons: ['OK']
-                });
-            } else {
-                dialog.showErrorBox('File Save Error', err);
-            }
-        });
-    }
+ipcMain.on('config-sent', (evt, confPresets) => {
+	var presets;
+	if (confPresets) {
+		if (!configFileToSave) return;
+		fs.writeFile(configFileToSave, JSON.stringify(confPresets[0]), function(err) {
+			if (!err) {
+				dialog.showMessageBox({
+					message: 'The file has been saved.',
+					buttons: ['OK']
+				});
+			} else {
+				dialog.showErrorBox('File Save Error', err);
+			}
+		});
+	}
 });
+// === End Of Export
+/* Configuration Import */
+function onConfigFileRead(err, data) {
+	var configuration;
+	if (err) throw err;
+	configuration = JSON.parse(data);
+	win.webContents.send('import-config', configuration);
+}
 
 function onImportConfig(fileName) {
-    //TODO update the current coniguration
-    if (!fileName) return;
-    fs.readFile(fileName[0], 'utf-8', onConfigFileRead);
-}
-
-function saveConfig() {
-    dialog.showSaveDialog({
-        title: 'Export configuration in file',
-        filters: [{
-            name: 'Config file',
-            extensions: ['json']
-        }]
-    }, function(fileName) {
-        win.webContents.send('request-config', fileName);
-    });
+	if (!fileName) return;
+	fs.readFile(fileName[0], 'utf-8', onConfigFileRead);
 }
 
 function importConfig() {
-    dialog.showOpenDialog({
-        title: 'Import configuration in file',
-        filters: [{
-            name: 'Config file',
-            extensions: ['json']
-        }]
-    }, onImportConfig);
+	dialog.showOpenDialog({
+		title: 'Import configuration in file',
+		filters: [{
+			name: 'Config file',
+			extensions: ['json']
+		}]
+	}, onImportConfig);
 }
+// === End Of Import
 
 function SetGlobalShortcut() {
-    var array = [...Array(12).keys()];
-    for (let i in array) { // eslint-disable-line prefer-const
-        const u = parseInt(i) + 1;
-        console.log('F' + u);
-        globalShortcut.register('F' + u, () => {
-            win.webContents.send('select-preset-index', i);
-        });
-    }
+	var array = [...Array(12).keys()];
+	for (let i in array) { // eslint-disable-line prefer-const
+		const u = parseInt(i) + 1;
+		console.log('F' + u);
+		globalShortcut.register('F' + u, () => {
+			win.webContents.send('select-preset-index', i);
+		});
+	}
 }
 
 function createWindow() {
-    SetGlobalShortcut();
-    // Create the browser window.
-    win = new BrowserWindow({
-        width: 1200,
-        height: 800
-    });
+	SetGlobalShortcut();
+	// Create the browser window.
+	win = new BrowserWindow({
+		width: 1200,
+		height: 800
+	});
 
-    const template = [{
-            label: 'File',
-            submenu: [{
-                role: 'quit'
-            }]
-        },
-        {
-            label: 'Edit',
-            submenu: [{
-                    label: 'Export config',
-                    click: saveConfig
-                },
-                {
-                    label: 'Import config',
-                    click: importConfig
-                }
-            ]
-        },
-        {
-            label: 'View',
-            submenu: [{
-                    label: 'Edition mode',
-                    accelerator: 'F9',
-                    click: toggleMode,
-                    checked: isInEditMode,
-                    type: 'checkbox'
-                },
-                {
-                    role: 'toggledevtools'
-                },
-                {
-                    role: 'togglefullscreen'
-                }
-            ]
-        },
-        {
-            role: 'reload',
-            accelerator: 'F5'
-        },
-        {
-            role: 'about'
-        }
-    ];
+	const template = [{
+			label: 'File',
+			submenu: [{
+				role: 'quit'
+			}]
+		},
+		{
+			label: 'Edit',
+			submenu: [{
+					label: 'Export config',
+					click: saveConfig
+				},
+				{
+					label: 'Import config',
+					click: importConfig
+				}
+			]
+		},
+		{
+			label: 'View',
+			submenu: [{
+					label: 'Edition mode',
+					accelerator: 'F9',
+					click: toggleMode,
+					checked: isInEditMode,
+					type: 'checkbox'
+				},
+				{
+					role: 'toggledevtools'
+				},
+				{
+					role: 'togglefullscreen'
+				}
+			]
+		},
+		{
+			role: 'reload',
+			accelerator: 'F5'
+		},
+		{
+			role: 'about'
+		}
+	];
 
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+	const menu = Menu.buildFromTemplate(template);
+	Menu.setApplicationMenu(menu);
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'dist/index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+	// and load the index.html of the app.
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'dist/index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
 
-    // See index.dev.js
+	// See index.dev.js
 
-    // Open the DevTools.
-    // win.webContents.openDevTools();
+	// Open the DevTools.
+	// win.webContents.openDevTools();
 
-    // add the vuejs extension
-    // BrowserWindow.addDevToolsExtension('resources/vuejs-devtool');
+	// add the vuejs extension
+	// BrowserWindow.addDevToolsExtension('resources/vuejs-devtool');
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null;
-    });
+	// Emitted when the window is closed.
+	win.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null;
+	});
 }
 
 // This method will be called when Electron has finished
@@ -173,19 +176,19 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow();
-    }
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (win === null) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const Vue = require('vue/dist/vue.js');
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.css';
 import {
-    createBlockSettings,
-    createPreset
+	createBlockSettings,
+	createPreset
 } from 'config';
 const electron = require('electron');
 
@@ -17,60 +17,58 @@ Vue.component('menu-toolbar', require('./components/menu-toolbar.vue'));
 Vue.component('edit-modal', require('./components/edit-modal.vue'));
 
 var mainView = new Vue({ // eslint-disable-line vars-on-top
-    el: '#mainView',
+	el: '#mainView',
 
-    data: {
-        isInEditMode: true,
-        showModal: false,
-        blockContext: null,
-        toastedMessage: null,
-        presets: [createPreset()],
-        currentPreset: null
-    },
-    created() {
-        this.currentPreset = this.presets[0];
-    },
-    methods: {
-        addBlock: function() {
-            this.currentPreset.blocks.push(createBlockSettings());
-        },
-
-        showToastedMessage: function(message) {
-            this.toastedMessage = message;
-            this.$refs.snackbar.open();
-        },
-        openModal: function(data) {
-            this.showModal = true;
-            this.viewData = data;
-        }
-    }
+	data: {
+		isInEditMode: true,
+		showModal: false,
+		blockContext: null,
+		toastedMessage: null,
+		presets: [createPreset()],
+		currentPreset: null
+	},
+	created() {
+		this.currentPreset = this.presets[0];
+	},
+	methods: {
+		addBlock: function() {
+			this.currentPreset.blocks.push(createBlockSettings());
+		},
+		showToastedMessage: function(message) {
+			this.toastedMessage = message;
+			this.$refs.snackbar.open();
+		},
+		openModal: function(data) {
+			this.showModal = true;
+			this.viewData = data;
+		}
+	}
 });
 ipc.on('edit-mode', (evt, active) => {
-    mainView._data.isInEditMode = active; // eslint-disable-line no-underscore-dangle
-    mainView.showToastedMessage(active ? 'Edition mode' : 'Display mode');
-    console.log('Receive signal to editmode: ' + active);
+	mainView._data.isInEditMode = active; // eslint-disable-line no-underscore-dangle
+	mainView.showToastedMessage(active ? 'Edition mode' : 'Display mode');
+	console.log('Receive signal to editmode: ' + active);
 });
-ipc.on('request-config', (evt, fileName) => {
-    var confPresets = JSON.stringify({
-        'fileName': fileName,
-        'presets': mainView.data.presets
-    });
-    event.sender.send('requested-config', confPresets);
+ipc.on('request-config', (evt) => {
+	ipc.send('config-sent', mainView.$data.presets);
+});
+ipc.on('import-config', (evt, config) => {
+	mainView.$data.presets = config;
 });
 ipc.on('select-preset-index', (evt, index) => {
-    if (mainView.$data.presets.length > index) {
-        mainView.$data.currentPreset = mainView.$data.presets[index];
-        mainView.showToastedMessage('Preset ' + mainView.$data.currentPreset.name + ' selected');
-    }
+	if (mainView.$data.presets.length > index) {
+		mainView.$data.currentPreset = mainView.$data.presets[index];
+		mainView.showToastedMessage('Preset ' + mainView.$data.currentPreset.name + ' selected');
+	}
 });
 bus.$on('open-webview-settings', function(blockContext) {
-    mainView.$data.blockContext = blockContext;
-    mainView.$refs.editmodal.$children[0].open();
+	mainView.$data.blockContext = blockContext;
+	mainView.$refs.editmodal.$children[0].open();
 });
 bus.$on('open-webview-settings', function(blockContext) {
-    mainView.$data.blockContext = blockContext;
-    mainView.$refs.editmodal.$children[0].open();
+	mainView.$data.blockContext = blockContext;
+	mainView.$refs.editmodal.$children[0].open();
 });
 bus.$on('select-preset', function(preset) {
-    mainView.$data.currentPreset = preset;
+	mainView.$data.currentPreset = preset;
 });
