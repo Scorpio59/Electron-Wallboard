e23d7185f54faa7d2a0bb027288ecf2ba53f1b1c
Scorpio59 <rudywahl@gmail.com>
2017-03-31 21:55:53 +0200

    add autosave and autoload of the config file

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const {
 	ipcMain
 } = electron;
 
-// import {app, BrowserWindow, Menu} from 'electron';
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
@@ -18,139 +17,149 @@ const fs = require('fs');
 let win;
 var isInEditMode = true;
 var configFileToSave;
-
+const defaultConfigFile = path.join(app.getPath('userData'), 'wallboard.config');
 function toggleMode() {
-	isInEditMode = !isInEditMode;
-	win.webContents.send('edit-mode', isInEditMode);
+  isInEditMode = !isInEditMode;
+  win.webContents.send('edit-mode', isInEditMode);
 }
 
 /* Configuration Export */
 function saveConfig() {
-	dialog.showSaveDialog({
-		title: 'Export configuration in file',
-		filters: [{
-			name: 'Config file',
-			extensions: ['json']
-		}]
-	}, function(fileName) {
-		configFileToSave = fileName;
-		win.webContents.send('request-config');
-	});
-}
+  dialog.showSaveDialog({
+    title: 'Export configuration in file',
+    filters: [{
+      name: 'Config file',
+      extensions: ['json']
+    }]
+  }, function (fileName) {
+    configFileToSave = fileName;
+    win.webContents.send('request-config');
+  });
+};
+
+function saveConfigFile(fileName, config) {
+  fs.writeFile(fileName, JSON.stringify(config), function (err) {
+    if (!err) {
+        // TODO Toaseted message'The file has been saved.',
+    } else {
+      dialog.showErrorBox('File Save Error', err);
+    }
+  });
+};
 
 ipcMain.on('config-sent', (evt, confPresets) => {
-	var presets;
-	if (confPresets) {
-		if (!configFileToSave) return;
-		fs.writeFile(configFileToSave, JSON.stringify(confPresets[0]), function(err) {
-			if (!err) {
-				dialog.showMessageBox({
-					message: 'The file has been saved.',
-					buttons: ['OK']
-				});
-			} else {
-				dialog.showErrorBox('File Save Error', err);
-			}
-		});
-	}
+  if (confPresets) {
+    if (configFileToSave) {
+      saveConfigFile(configFileToSave, confPresets);
+      configFileToSave = null;
+    } else {
+      saveConfigFile(defaultConfigFile, confPresets);
+    }
+  }
 });
+
 // === End Of Export
 /* Configuration Import */
 function onConfigFileRead(err, data) {
-	var configuration;
-	if (err) throw err;
-	configuration = JSON.parse(data);
-	win.webContents.send('import-config', configuration);
+  var configuration;
+  if (err) throw err;
+  configuration = JSON.parse(data);
+  win.webContents.send('import-config', configuration);
 }
 
 function onImportConfig(fileName) {
-	if (!fileName) return;
-	fs.readFile(fileName[0], 'utf-8', onConfigFileRead);
+  if (!fileName || !fs.existsSync(fileName)) return;
+  fs.readFile(fileName, 'utf-8', onConfigFileRead);
 }
 
 function importConfig() {
-	dialog.showOpenDialog({
-		title: 'Import configuration in file',
-		filters: [{
-			name: 'Config file',
-			extensions: ['json']
-		}]
-	}, onImportConfig);
+  dialog.showOpenDialog({
+    title: 'Import configuration in file',
+    defaultPath: configFileToSave,
+    filters: [{
+      name: 'Config file',
+      extensions: ['json']
+    }]
+  }, (files) => onImportConfig(files[0]));
 }
 // === End Of Import
 
 function SetGlobalShortcut() {
-	var array = [...Array(12).keys()];
-	for (let i in array) { // eslint-disable-line prefer-const
-		const u = parseInt(i) + 1;
-		console.log('F' + u);
-		globalShortcut.register('F' + u, () => {
-			win.webContents.send('select-preset-index', i);
-		});
-	}
+  var array = [...Array(8).keys()];
+  for (let i in array) { // eslint-disable-line prefer-const
+    const u = parseInt(i) + 1;
+    globalShortcut.register('F' + u, () => {
+      win.webContents.send('select-preset-index', i);
+    });
+  }
 }
 
 function createWindow() {
-	SetGlobalShortcut();
+  SetGlobalShortcut();
 	// Create the browser window.
-	win = new BrowserWindow({
-		width: 1200,
-		height: 800
-	});
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    show: false
+  });
 
-	const template = [{
-			label: 'File',
-			submenu: [{
-				role: 'quit'
-			}]
-		},
-		{
-			label: 'Edit',
-			submenu: [{
-					label: 'Export config',
-					click: saveConfig
-				},
-				{
-					label: 'Import config',
-					click: importConfig
-				}
-			]
-		},
-		{
-			label: 'View',
-			submenu: [{
-					label: 'Edition mode',
-					accelerator: 'F9',
-					click: toggleMode,
-					checked: isInEditMode,
-					type: 'checkbox'
-				},
-				{
-					role: 'toggledevtools'
-				},
-				{
-					role: 'togglefullscreen'
-				}
-			]
-		},
-		{
-			role: 'reload',
-			accelerator: 'F5'
-		},
-		{
-			role: 'about'
-		}
-	];
+  const template = [{
+    label: 'File',
+    submenu: [{
+      role: 'quit'
+    }]
+  },
+  {
+    label: 'Edit',
+    submenu: [{
+      label: 'Export config',
+      click: saveConfig
+    },
+    {
+      label: 'Import config',
+      click: importConfig
+    }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [{
+      label: 'Edition mode',
+      accelerator: 'F9',
+      click: toggleMode,
+      checked: isInEditMode,
+      type: 'checkbox'
+    },
+    {
+      role: 'toggledevtools'
+    },
+    {
+      role: 'togglefullscreen'
+    }
+    ]
+  },
+  {
+    role: 'reload',
+    accelerator: 'F5'
+  },
+  {
+    role: 'about'
+  }
+  ];
 
-	const menu = Menu.buildFromTemplate(template);
-	Menu.setApplicationMenu(menu);
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
 	// and load the index.html of the app.
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'dist/index.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  win.once('ready-to-show', () => {
+    onImportConfig(defaultConfigFile);
+    win.show();
+  });
 
 	// See index.dev.js
 
@@ -161,12 +170,12 @@ function createWindow() {
 	// BrowserWindow.addDevToolsExtension('resources/vuejs-devtool');
 
 	// Emitted when the window is closed.
-	win.on('closed', () => {
+  win.on('closed', () => {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		win = null;
-	});
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -178,17 +187,17 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (win === null) {
-		createWindow();
-	}
+  if (win === null) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
