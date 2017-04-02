f94edd00a112e19317b9fdba77b457cfc8856876
Scorpio59 <rudywahl@gmail.com>
2017-04-02 16:14:57 +0200

    add auto zomm and auto scrool to webview selector

--- src/components/web-view.vue
+++ src/components/web-view.vue
@@ -1,7 +1,7 @@
 <template>
 <section id="web-view" style="height:100%" :class="{edit : isInEditMode}" class="block md-accent">
-    <webview :src="currentTab.url" style="height:100%" :class="{edit : isInEditMode }" class="hideOnHover"></webview>
-    <div  class="help displayOnHover">Move and resize me</div>
+    <webview :src="currentTab.url" style="height:100%" :class="{edit : isInEditMode }" class=""></webview>
+    <!-- <div  class="help displayOnHover">Move and resize me</div>-->
     <div class="md-fab  md-dense md-fab-top-left edition">
         <md-button v-if="isInEditMode" class="md-icon-button md-mini md-raised md-primary" @click.native="editBlock">
             <md-icon>edit</md-icon>
@@ -23,11 +23,19 @@ export default {
         return {
             currentTab : this.blockContext.tabs[0],
             currentIndex : 0,
-            intervalID: null
+            intervalID: null,
+            webview:null
         };
     },
     created() {
       this.intervalID = window.setInterval(this.nextTab, this.blockContext.rollingTime * 1000);
+
+    },
+    mounted(){
+      this.webview = this.$el.getElementsByTagName('webview')[0];
+      this.webview.addEventListener('did-finish-load', ()=>{
+        this.autoZoom();
+      });
     },
     methods: {
         deleteBlock: function() {
@@ -43,17 +51,57 @@ export default {
         nextTab:function(){
           this.currentIndex=(this.currentIndex+1) % this.blockContext.tabs.length;
           this.currentTab =  this.blockContext.tabs[this.currentIndex];
-        }
-
+        },
+        autoZoom:function(){
+          if(this.currentTab.selector)
+          {
+            let code = `var element = document.getElementById("`+ this.currentTab.selector+`");
+            rect = element.getBoundingClientRect()
+                        JSON.stringify({top:rect.top,right:rect.right,bottom:rect.bottom,left:rect.left,height:rect.height,width:rect.width})`;
+            this.webview.getWebContents().executeJavaScript(code,false).then((result) =>{
+              var clientRect = JSON.parse(result);
+              if(this.currentTab.autoZoom)
+              {
+                var webviewHeight = this.webview.getBoundingClientRect().height;
+                var webviewWidth = this.webview.getBoundingClientRect().width;
+                var zoomFactorHeight = webviewHeight / clientRect.height;
+                var zoomFactorWidth = webviewWidth / clientRect.width;
+                var zoomFactor = Math.min(zoomFactorWidth,zoomFactorHeight);
+                this.webview.setZoomFactor(zoomFactor);
+              }
+              setTimeout(() => {
+                    this.webview.getWebContents().executeJavaScript(code,false).then((result) =>{
+                          var clientRect = JSON.parse(result);
+                          this.webview.getWebContents().executeJavaScript("document.body.scrollLeft = "+ clientRect.left+"; document.body.scrollTop = "+ clientRect.top+";  ");
+                    });
+                  }, 100)
+              });
+            }
+          }
     },
     watch: {
        blockContext:{
-         handler:function(){
-         clearInterval(this.intervalID);
-         this.intervalID = window.setInterval(this.nextTab, this.blockContext.rollingTime * 1000);
-       },
+         handler: _.debounce(function(){
+             clearInterval(this.intervalID);
+             this.intervalID = window.setInterval(this.nextTab, this.blockContext.rollingTime * 1000);
+           },1000),
          deep: true
-       }
+       },
+       isInEditMode: function(newValue)  {
+          if(newValue)
+          {
+            this.webview.insertCSS("body{ overflow:auto;}")
+          }else {
+            //hide the scrollbar
+            this.webview.insertCSS("body{ overflow:hidden;}")
+          }
+        },
+        currentTab: {
+          handler: _.debounce(function(){
+              this.autoZoom()
+            },1000),
+          deep: true
+        },
      }
 };
 
