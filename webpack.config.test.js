65a26b4b08ac148e73c45965875b33599d6e991f
Eric RENARD <timmy469@gmail.com>
2017-04-13 08:52:46 +0200

    Adds coverage of .js

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,11 +1,31 @@
-var testConfig = {
+const path = require('path');
+const buildConfig = require('./webpack.config.js');
+
+const istanbulIncludes = [];
+buildConfig.resolve.modules.forEach((relativePath) => {
+  if (relativePath.indexOf('node_modules') === -1) {
+    istanbulIncludes.push(path.resolve(relativePath));
+  }
+});
+
+const testConfig = {
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
   externals: [require('webpack-node-externals')()], // in order to ignore all modules in node_modules folder
+  devtool: 'inline-cheap-module-source-map',
+  resolve: buildConfig.resolve,
   module: {
     rules: [
       {
         test: /\.js$/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.(js|ts)/,
+        include: istanbulIncludes, // instrument only testing sources with Istanbul, after ts-loader runs
+        loader: 'istanbul-instrumenter-loader',
+        options: {
+          esModules: true
+        }
       }
     ]
   }
